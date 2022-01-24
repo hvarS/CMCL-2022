2a1e8bd9768a6fa0bce496a6163969f79d1b3b27
Harshvardhan <srivastavahv@gmail.com>
2022-01-24 13:23:23 +0530

    Seperated Text Name from Sentence_Id

--- train_data_preprocessor.py
+++ train_data_preprocessor.py
@@ -1,7 +1,24 @@
 import pandas as pd
 
-file = pd.read_csv('training_data2022/training_data/train.csv')
-dev_file = pd.read_csv('training_data2022/training_data/dev.csv')
+def get_sentence_number(s):
+    elements= s.split('-')
+    print(elements)
+    return int(elements[-1])
 
+def get_text_name(s):
+    elements= s.split('-')
+    return '-'.join(elements[:-1])
+
+file = pd.read_csv('data/training_data/train.csv')
+file = file[['language','sentence_id','word_id','word','FFDAvg','FFDStd','TRTAvg','TRTStd','language_sentence_id','text_name']]
+dev_file = pd.read_csv('data/training_data/dev.csv')
+dev_file = dev_file[['language','sentence_id','word_id','word','FFDAvg','FFDStd','TRTAvg','TRTStd','language_sentence_id','text_name']]
 df = pd.concat([file,dev_file])
-df.to_csv('training_data2022/training_data/train_and_valid.csv',index = False)
+
+df['text_name'] = df['language_sentence_id'].apply(get_text_name)
+file['text_name'] = file['language_sentence_id'].apply(get_text_name)
+dev_file['text_name'] = dev_file['language_sentence_id'].apply(get_text_name)
+df.to_csv('data/training_data/train_and_valid.csv',index = False)
+file.to_csv('data/training_data/train.csv',index = False)
+dev_file.to_csv('data/training_data/dev.csv',index = False)
+print(df['text_name'].unique())
