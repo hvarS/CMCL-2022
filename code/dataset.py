f6611f752bac5644fb1479ddf2d8b0f1c85e094d
Harshvardhan <srivastavahv@gmail.com>
2022-01-29 19:28:53 +0530

    changed base model to xlm roberta

--- code/dataset.py
+++ code/dataset.py
@@ -5,7 +5,7 @@ from transformers import AutoTokenizer
 MAX_LEN = 128
 
 class TransduciveDataset(Dataset):
-    def __init__(self,texts,labels,tf_name = 'bert-base-uncased') -> None:
+    def __init__(self,texts,labels,tf_name = 'xlm-roberta-base') -> None:
         super(TransduciveDataset,self).__init__()
         try:
             assert len(texts) == len(labels)
@@ -21,10 +21,10 @@ class TransduciveDataset(Dataset):
         encoded_inputs = self.tokenizer(self.texts[index],padding = 'max_length',is_split_into_words = True,max_length = MAX_LEN,truncation = True,return_tensors='pt')
         labels = torch.zeros(MAX_LEN,4)
         decoded_texts = self.tokenizer.convert_ids_to_tokens(encoded_inputs.input_ids[0])
-        if 'bert' in self.tf_name:
+        if not 'xlm' in self.tf_name:
             word_mask = [t!='[CLS]' and t!='[SEP]' and t!='[PAD]' and t[0]!='#' for t in decoded_texts]
         else:
-            word_mask = [True for _ in decoded_texts]
+            word_mask = [t[0]=='▁' for t in decoded_texts]
         try:
             labels[word_mask] = torch.tensor(self.labels[index],dtype = torch.float) #[4,]
         except RuntimeError:
